6e9921bcc6cb9f9d674706bd7ecab551225d8248
Henry Phan <h.phan06@gmail.com>
2019-11-12 19:54:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# def square_array(array)
+#   na = []
+#   array.each {|a| na << a ** 2}
+#   na
+# end
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |numbers|
+    new_array << numbers**2 
+  end
+  new_array
 end
